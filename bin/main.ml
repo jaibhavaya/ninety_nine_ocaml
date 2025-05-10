c376028da2af523ab7b43d579f3e5861964f2fb1
Jacob <jaibhavaya@gmail.com>
2025-05-10 02:08:34 -0600

    restructure

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,4 @@
+(* 1. last item*)
 let rec last list =
   match list with
   | [] -> None
@@ -5,26 +6,33 @@ let rec last list =
   | _ :: tail -> last tail
 
 let () =
- let list = [1; 2; 3; 4] in
-  match last list with
-  | None -> print_endline "The list is empty"
-  | Some x -> print_endline ("The last element is " ^ string_of_int x)
-
-let test_cases () =
-  let _ = match last [] with
-    | None -> print_endline "Empty list test passed"
-    | Some _ -> print_endline "Empty list test failed"
-  in
-
-  let _ = match last [42] with
-    | Some 42 -> print_endline "Single element test passed"
-    | _ -> print_endline "Single element test failed"
-  in
-
-  let _ = match last [1; 2; 3; 4; 5] with
-    | Some 5 -> print_endline "Multiple elements test passed"
-    | _ -> print_endline "Multiple elements test failed"
+  let _ = match last [4;2] with
+    | Some 2 -> print_endline "Last item test passed"
+    | _ -> print_endline "Last item test failed"
   in
   ()
 
-let () = test_cases ()
+(* 2. last two items *)
+let rec last_two list =
+  match list with
+  | [] | [_] -> None
+  | [x;y] -> Some (x,y)
+  | _ :: tail -> last_two tail
+
+let () =
+ let list = [1; 2; 3; 4] in
+  match last_two list with
+  | Some (3,4) -> print_endline "Last two items test passed"
+  | _ -> print_endline "Last item test failed"
+
+(* 3. item at k *)
+let rec at k = function
+  | [] -> None
+  | h :: t -> if k = 0 then Some h else at (k-1) t
+
+let () =
+ let list = [1; 2; 3; 4] in
+  match at 2 list with
+      | Some 3 -> print_endline "Item at k test passed"
+      | _ -> print_endline "Item at k  test failed"
+
